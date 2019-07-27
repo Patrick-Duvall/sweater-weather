14e31ee73af8cedac75910646ce6abedafd94584
Patrick Duvall <duvallp208@gmail.com>
2019-07-27 15:38:16 -0600

    Add forecast service, still need to serialize

---
+++ app/services/darksky_forecast_service.rb
@@ -0,0 +1,25 @@
+class DarkskyForecastService
+
+  def self.forecast(lat,lng)
+    new.forecast(lat,lng)
+  end
+
+  def forecast(lat, lng)
+    path = "/forecast/#{ENV['DARKSKY_API_KEY']}/#{lat},#{lng}"
+    response = conn.get(path)
+    info = JSON.parse(response.body)
+    {
+      'currently' => info['currently'],
+      'hourly' => info['hourly'],
+      'daily' => info['daily']
+    }
+  end
+
+private
+  def conn
+    Faraday.new("https://api.darksky.net") do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+end
--- spec/services/darksky_forecast_service_spec.rb
+++ spec/services/darksky_forecast_service_spec.rb
@@ -4,13 +4,26 @@ describe "darksky forecast service" do
   #TODO not use real API
   it ".forecast" do
     service = DarkskyForecastService.new
-    forecast = service.forcast([39.7392358, -104.990251])
+    forecast = service.forecast(39.7392358, -104.990251)
     expect(forecast['currently']).to be_a(Hash)
     expect(forecast['currently']).to have_key('summary')
     expect(forecast['hourly']).to be_a(Hash)
     expect(forecast['hourly']).to have_key('summary')
+    expect(forecast['hourly']).to have_key('data')
     expect(forecast['daily']).to be_a(Hash)
     expect(forecast['daily']).to have_key('summary')
+    expect(forecast['daily']).to have_key('data')
+  end
+  it "#forecast" do
+    forecast = DarkskyForecastService.forecast(39.7392358, -104.990251)
+    expect(forecast['currently']).to be_a(Hash)
+    expect(forecast['currently']).to have_key('summary')
+    expect(forecast['hourly']).to be_a(Hash)
+    expect(forecast['hourly']).to have_key('summary')
+    expect(forecast['hourly']).to have_key('data')
+    expect(forecast['daily']).to be_a(Hash)
+    expect(forecast['daily']).to have_key('summary')
+    expect(forecast['daily']).to have_key('data')
   end
 
 end
