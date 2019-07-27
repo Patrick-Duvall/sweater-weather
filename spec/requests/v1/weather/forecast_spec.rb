5968453180835d3f891f2ce1a88dff82098956d5
Patrick Duvall <duvallp208@gmail.com>
2019-07-27 14:59:55 -0600

    Able to get info through form endpoint, need to refactor to service and serializer

--- spec/requests/v1/weather/forecast_spec.rb
+++ spec/requests/v1/weather/forecast_spec.rb
@@ -5,16 +5,12 @@ describe "weather forecast controller" do
   it "shows a summary" do
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
-    expect(response['currently']).to_not be_falsey
-  end
-  it "shows hourly forecast" do
-    expect(response).to be_successful
-    expect(response['hourly']).to_not be_falsey
+    info = JSON.parse(response.body)
+    expect(info['data']['currently']).to_not be_falsey
+    expect(info['data']['hourly']).to_not be_falsey
+    expect(info['data']['daily']).to_not be_falsey
 
   end
-  it "shows daily forecast" do
-    expect(response).to be_successful
-    expect(response['daily']).to_not be_falsey
-  end
+
 
 end
