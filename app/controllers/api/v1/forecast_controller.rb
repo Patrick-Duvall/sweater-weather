c9e24898985c8286841f135694cb531554818365
Patrick Duvall <duvallp208@gmail.com>
2019-07-27 16:03:37 -0600

    format forecast

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,12 +3,8 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     latlng = GoogleGeocodingService.latlng(params['location'])
-    conn = Faraday.new(:url => 'https://api.darksky.net/forecast/68d20c6de84d5448a56e180036ab5e62/39.7392358,-104.990251') do |faraday|
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-    response = conn.get
-
-    render json: JSON.parse(response.body), status: 200
+    forecast = DarkskyForecastService.forecast(latlng[0],latlng[1])
+    render json: {'data' => forecast}, status: 200
   end
 
 end
