5a19d058a82a2911df62ff31e815aff30dacffc0
Patrick Duvall <duvallp208@gmail.com>
2019-07-28 09:08:11 -0600

    Add basic Flickr service that gets urls

--- app/services/flickr_image_service.rb
+++ app/services/flickr_image_service.rb
@@ -2,12 +2,13 @@ class FlickrImageService
 
   def get_photos(lat, lng, limit)
     response =  conn.get do |req|
+                  req.url ''
                   req.params['lat'] = lat
                   req.params['lon'] = lng
                   req.params['per_page'] = limit
                 end
     info = JSON.parse(response.body)
-    require "pry"; binding.pry
+    info['photos']['photo'].map{|photo_info| photo_info['url_o']}
   end
 
 private
@@ -21,6 +22,8 @@ private
       faraday.params['extras'] = 'url_o'
       faraday.params['format'] = 'json'
       faraday.params['safe_search'] = '0'
+      faraday.params['nojsoncallback'] = '1'
+      faraday.params['tag_mode'] = 'all'
     end
   end
 
--- spec/services/flickr_image_service_spec.rb
+++ spec/services/flickr_image_service_spec.rb
@@ -4,8 +4,8 @@ describe "flickr image service" do
   it ".get_image" do
     service = FlickrImageService.new
     expect(service).to be_a(FlickrImageService)
-    expect(service.get_photos("denver,co", 1)).to be_a(Array)
-    expect(service.get_photos("denver,co", 1)[0]).to match("https")
+    expect(service.get_photos(39.7392358, -104.990251, 2)).to be_a(Array)
+    expect(service.get_photos(39.7392358, -104.990251, 1)[0]).to match("https")
   end
 
 end
