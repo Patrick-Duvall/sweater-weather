a13cc47f2d5bd59380e0afc241051516dcc0a687
Patrick Duvall <duvallp208@gmail.com>
2019-07-28 16:51:39 -0600

    Add forecast serializer

--- spec/serializers/forecast_serializer_spec.rb
+++ spec/serializers/forecast_serializer_spec.rb
@@ -5,9 +5,10 @@ describe "ForecastSerializer " do
     info = File.read('fixtures/weather_for_a_day_denver.json')
     forecast_info = JSON.parse(info)
     forecast = Forecast.new(forecast_info, 'denver,co')
-    result = ForecastSerializer.present_forcast(forecast)
-    expect(result['data']['currently']['summary']).to eq("Mostly Cloudy")
-    expect(result['data']['hourly']).count.to eq(5)
-    expect(result['data']['daily']).count.to eq(5)
+    result = ForecastSerializer.present_forecast(forecast)
+    expect(result['data']['current']['summary']).to eq("Possible light rain this evening.")
+    expect(result['data']['current']['brief_summary']).to eq("Mostly Cloudy")
+    expect(result['data']['hourly'].count).to eq(8)
+    expect(result['data']['daily'].count).to eq(5)
   end
 end
