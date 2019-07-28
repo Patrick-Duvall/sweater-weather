4a5a1ecbf1906cd47ab8ede32ba9de6e913257e2
Patrick Duvall <duvallp208@gmail.com>
2019-07-28 16:13:21 -0600

    Refactor model to have current method, ivar for weather info

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -12,17 +12,24 @@ class Forecast
               :summary
   def initialize(weather_info, cityState)
     @weather_info = weather_info
-    @city = cityState.split(',')[0].capitalize
-    @state = cityState.split(',')[1].upcase
-    @brief_summary = weather_info['currently']['summary']
-    @temp = weather_info['currently']['temperature']
-    @temp_high = weather_info['daily']['data'][0]['temperatureHigh']
-    @temp_low = weather_info['daily']['data'][0]['temperatureLow']
-    @feels_like = weather_info['currently']['temperature'] - weather_info['currently']['windSpeed']
-    @humidity = weather_info['currently']['humidity']
-    @visibility = weather_info['currently']['visibility']
-    @uv_index = weather_info['currently']['uvIndex']
-    @summary = weather_info['hourly']['summary']
+    @location = cityState
+  end
+
+  def current
+    current = @weather_info
+      {
+        'city' => @location.split(',')[0].capitalize,
+        'state' => @location.split(',')[1].upcase,
+        'brief_summary' => @weather_info['currently']['summary'],
+        'temp' => @weather_info['currently']['temperature'],
+        'temp_high' => @weather_info['daily']['data'][0]['temperatureHigh'],
+        'temp_low' => @weather_info['daily']['data'][0]['temperatureLow'],
+        'feels_like' => @weather_info['currently']['temperature'] - @weather_info['currently']['windSpeed'],
+        'humidity' => @weather_info['currently']['humidity'],
+        'visibility' => @weather_info['currently']['visibility'],
+        'uv_index' => @weather_info['currently']['uvIndex'],
+        'summary' => @weather_info['hourly']['summary']
+      }
   end
 
   def hourly
