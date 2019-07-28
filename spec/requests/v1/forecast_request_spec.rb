dc8527d4c6e8867cd5f2bff9c7f091089ef41710
Patrick Duvall <duvallp208@gmail.com>
2019-07-28 16:57:59 -0600

    Reformat forecast controller to use forecast serializer

--- spec/requests/v1/forecast_request_spec.rb
+++ spec/requests/v1/forecast_request_spec.rb
@@ -6,7 +6,7 @@ describe "weather forecast controller" do
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
     info = JSON.parse(response.body)
-    expect(info['data']['currently']).to_not be_falsey
+    expect(info['data']['current']).to_not be_falsey
     expect(info['data']['hourly']).to_not be_falsey
     expect(info['data']['daily']).to_not be_falsey
 
