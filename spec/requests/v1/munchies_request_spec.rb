75a5c6d0aef2c67916cecb82ca35c6748401daff
Patrick Duvall <duvallp208@gmail.com>
2019-07-29 09:19:35 -0600

    add spec, need to authorize api key

---
+++ spec/requests/v1/munchies_request_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+describe "munchies api" do
+  it "returns 3 restaurants in a destination city" do
+    get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese Content-Type: application/json Accept: application/json'
+    expect(response).to be_successful
+    info = JSON.parse(response.body)
+    expect(info['data']['destination']).to_not be_falsey
+    expect(info['data']['restaurants'].count).to eq(3)
+    expect(info['data']['restaurants'][0]['address']).to_not be_falsey
+    expect(info['data']['restaurants'][0]['name']).to_not be_falsey
+
+  end
+
+end
