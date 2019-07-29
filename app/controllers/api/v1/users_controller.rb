f8e1dd2d3d46b5a6e1afd8ca9f7712d96545112d
Patrick Duvall <duvallp208@gmail.com>
2019-07-29 14:50:55 -0600

    Add more robust feature spec

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
+    if params["password"] != params['password_confirmation']
+      render json: {"data": "Passwords do not match."}
+    elsif User.find_by(email: params['email'])
+      render json: {"data": "That email is in use."}
+    else
     user = User.create(email: params['email'], password: params['password'])
     render json: {"api_key": user.api_key}
+    end
   end
 
 end
