b06f38fe7d6e3325971c9dd3a09dd379b8354979
Patrick Duvall <duvallp208@gmail.com>
2019-07-29 14:58:50 -0600

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of :password }
+  end
+
+   it "has an api_key" do
+     user = User.create!(email: 'a@b.com', password: '123pkd')
+     expect(user.api_key).to be_a(String)
+     expect(user.api_key.length).to eq(32)
+   end
 end
