ebadf278365a82ca34325a2d19c57f676bde4868
Patrick Duvall <duvallp208@gmail.com>
2019-07-29 13:41:48 -0600

    make post request work

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::UsersController < ApplicationController
 
+  def create
+    require "pry"; binding.pry
+    
+  end
+
 end
--- spec/requests/v1/create_user_request_spec.rb
+++ spec/requests/v1/create_user_request_spec.rb
@@ -4,15 +4,31 @@ require "rails_helper"
 
 describe "weather forecast controller" do
 
+
+
   it "shows a summary" do
-    post '/api/v1/users'
-    require "pry"; binding.pry
+
+    body = {
+    "email": "whatever@example.com",
+    "password": "password",
+    "password_confirmation": "password"
+    }
+
+    headers = {'Content-Type': 'application/json',
+    'Accept': 'application/json'}
+
+
+  post '/api/v1/users', headers: headers, params: body.to_json
     expect(response).to be_successful
     info = JSON.parse(response.body)
     #NOT JSON 1.0 BUT Follows spec example to a T
     expect(info['api_key']).to_not be_falsey
 
   end
+
+  # conn = Faraday.new(:url => '/api/v1/users') do |faraday|
+  #   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  # end
 
 
 end
