98ab924d460a95333ba14494884e8185d69f7002
Patrick Duvall <duvallp208@gmail.com>
2019-07-29 11:18:37 -0600

    remove extra comments

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,13 +4,6 @@ class Api::V1::MunchiesController < ApplicationController
     google_directions_service = GoogleDirectionsService.new(params['start'], params['end'])
     arrival_time = google_directions_service.get_time
 
-    # directions_info = Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{params['start']}&destination=#{params['end']}&key=#{ENV['GOOGLE_API_KEY']}")
-
-    # directions = JSON.parse(directions_info.body)
-    # time_to_travel = directions['routes'].first['legs'].first['duration']['value']
-    # current_time = Time.now.to_i
-    # arrival_time = time_to_travel + current_time
-
     yelp_service = YelpService.new(arrival_time, params['end'], params['food'])
     restaurants = yelp_service.get_restaurants
 
