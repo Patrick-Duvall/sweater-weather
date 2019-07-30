7f55c75c8aabf2c8f38d21cdcf1c240e21360103
Patrick Duvall <duvallp208@gmail.com>
2019-07-30 12:00:33 -0600

    Refactor Darksky forecast spec to use webmock

--- spec/services/darksky_forecast_service_spec.rb
+++ spec/services/darksky_forecast_service_spec.rb
@@ -1,29 +1,35 @@
 require "rails_helper"
 
 describe "darksky forecast service" do
+
+  before :each do
+    @forecast = File.read('fixtures/forecast_denver_co.json')
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/39.7392358, -104.990251").with( body: @forecast)
+    @forecast = DarkskyForecastService.forecast(39.7392358, -104.990251)
+  end
   #TODO not use real API
   it ".forecast" do
     service = DarkskyForecastService.new
-    forecast = service.forecast(39.7392358, -104.990251)
-    expect(forecast['currently']).to be_a(Hash)
-    expect(forecast['currently']).to have_key('summary')
-    expect(forecast['hourly']).to be_a(Hash)
-    expect(forecast['hourly']).to have_key('summary')
-    expect(forecast['hourly']).to have_key('data')
-    expect(forecast['daily']).to be_a(Hash)
-    expect(forecast['daily']).to have_key('summary')
-    expect(forecast['daily']).to have_key('data')
+    @forecast = service.forecast(39.7392358, -104.990251)
+    expect(@forecast['currently']).to be_a(Hash)
+    expect(@forecast['currently']).to have_key('summary')
+    expect(@forecast['hourly']).to be_a(Hash)
+    expect(@forecast['hourly']).to have_key('summary')
+    expect(@forecast['hourly']).to have_key('data')
+    expect(@forecast['daily']).to be_a(Hash)
+    expect(@forecast['daily']).to have_key('summary')
+    expect(@forecast['daily']).to have_key('data')
   end
   it "#forecast" do
-    forecast = DarkskyForecastService.forecast(39.7392358, -104.990251)
-    expect(forecast['currently']).to be_a(Hash)
-    expect(forecast['currently']).to have_key('summary')
-    expect(forecast['hourly']).to be_a(Hash)
-    expect(forecast['hourly']).to have_key('summary')
-    expect(forecast['hourly']).to have_key('data')
-    expect(forecast['daily']).to be_a(Hash)
-    expect(forecast['daily']).to have_key('summary')
-    expect(forecast['daily']).to have_key('data')
+    @forecast = DarkskyForecastService.forecast(39.7392358, -104.990251)
+    expect(@forecast['currently']).to be_a(Hash)
+    expect(@forecast['currently']).to have_key('summary')
+    expect(@forecast['hourly']).to be_a(Hash)
+    expect(@forecast['hourly']).to have_key('summary')
+    expect(@forecast['hourly']).to have_key('data')
+    expect(@forecast['daily']).to be_a(Hash)
+    expect(@forecast['daily']).to have_key('summary')
+    expect(@forecast['daily']).to have_key('data')
   end
 
 end
