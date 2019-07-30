684b050cab4a4846d63d78d06831eec89218fa9e
Patrick Duvall <duvallp208@gmail.com>
2019-07-30 13:09:22 -0600

    Add webmocks for all service specs

--- spec/services/google_geocoding_service_spec.rb
+++ spec/services/google_geocoding_service_spec.rb
@@ -2,6 +2,13 @@ require "rails_helper"
 
 describe "Google Geocoding Service" do
   #TODO not use real API
+
+  before :each do
+    city_location = File.read('fixtures/denver_flicker_5_images.json')
+    stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=denver%2Cco&key=#{ENV['GOOGLE_API_KEY']}").with( body: city_location) 
+
+  end
+
   it ".latlng" do
     service = GoogleGeocodingService.new
     expect(service).to be_a(GoogleGeocodingService)
