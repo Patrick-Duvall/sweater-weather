448713a459c7421fa51d2bdc33987e8280b44633
Patrick Duvall <duvallp208@gmail.com>
2019-07-30 10:15:26 -0600

    Add protections for no or bad api_key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,13 +2,24 @@ class Api::V1::RoadTripController < ApplicationController
   protect_from_forgery with: :null_session
 
   def show
-    time = GoogleDirectionsService.get_time(params['origin'], params['destination'])
-    latlng = GoogleGeocodingService.latlng(params['origin'])
-    forecast_time = time.seconds + Time.now.to_i
-    forecast_info = DarkskyForecastService.future_forecast(latlng[0], latlng[1], forecast_time)
-    forecast = Forecast.new(forecast_info, params['destination'])
-    forecast_to_present = RoadTripSerializer.present_forecast(forecast, time)
-    render json: forecast_to_present, status: 200
+    if bad_api_key?(params['api_key'])
+      render json: {"data":"Unauthorized Request"}, status: 401
+    else
+      time = GoogleDirectionsService.get_time(params['origin'], params['destination'])
+      latlng = GoogleGeocodingService.latlng(params['origin'])
+      forecast_time = time.seconds + Time.now.to_i
+      forecast_info = DarkskyForecastService.future_forecast(latlng[0], latlng[1], forecast_time)
+      forecast = Forecast.new(forecast_info, params['destination'])
+      forecast_to_present = RoadTripSerializer.present_forecast(forecast, time)
+      render json: forecast_to_present, status: 200
+    end
   end
+
+  private
+
+  def bad_api_key?(key)
+    User.find_by(api_key: key).nil?
+  end
+
 
 end
--- spec/requests/v1/road_trip_request_spec.rb
+++ spec/requests/v1/road_trip_request_spec.rb
@@ -3,6 +3,13 @@
 require "rails_helper"
 
 describe "road trip controller" do
+
+  before :each do
+    User.create(email: 'abc@gmail.com',
+                password: 'a343',
+                api_key: "jgn983hy48thw9begh98h4539h4")
+  end
+
   it "returns a forcast for a destination" do
     body = {
       "origin": "Denver,CO",
@@ -16,9 +23,36 @@ describe "road trip controller" do
     post '/api/v1/road_trip', headers: headers, params: body.to_json
     expect(response).to be_successful
     info = JSON.parse(response.body)
-    require "pry"; binding.pry
     expect(info['data']['attributes']['forecast_on_arrival']).to_not be_falsey
     expect(info['meta']['travel_time_to']).to_not be_falsey
     expect(info['data']['id']).to eq('1')
   end
+
+  it "returns a 401 if no api_key" do
+    body = {
+      "origin": "Denver,CO",
+      "destination": "Pueblo,CO"
+    }
+    headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    }
+    post '/api/v1/road_trip', headers: headers, params: body.to_json
+    expect(response.status).to eq(401)
+  end
+
+  it "returns a 401 if bad api_key" do
+    body = {
+      "origin": "Denver,CO",
+      "destination": "Pueblo,CO",
+      "api_key": "abc123"
+    }
+    headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    }
+    post '/api/v1/road_trip', headers: headers, params: body.to_json
+    expect(response.status).to eq(401)
+  end
+
 end
