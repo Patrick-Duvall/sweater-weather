359f94c20696d978e412dd9620950efda470f349
Patrick Duvall <duvallp208@gmail.com>
2019-07-30 14:13:38 -0600

    Add city with update

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,15 @@
 class City < ApplicationRecord
+  after_create :update_info
+
+
+  private
+  def update_info
+    response = GoogleGeocodingService.get_city_info(querystring)
+    city = response['results'].first['address_components'].first['long_name']
+    state = response['results'].first['address_components'].third['short_name']
+    country = response['results'].first['address_components'].fourth['short_name']
+    lat = response['results'].first['geometry']['location']['lat']
+    lng = response['results'].first['geometry']['location']['lng']
+    self.update(city: city, state: state, country: country, lat:lat, lng:lng)
+  end
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe City, type: :model do
 
   it "has attributes after create" do
 
-    city = City.create('denver,co')
-    expect(city.name).to eq('Denver')
+    city = City.create!(querystring: 'denver,co')
+    expect(city.city).to eq('Denver')
     expect(city.state).to eq('CO')
-    expect(city.query_string).to eq('denver,co')
+    expect(city.country).to eq('US')
+    expect(city.querystring).to eq('denver,co')
     expect(city.lat).to eq(39.7392358)
     expect(city.lng).to eq(-104.990251)
   end
