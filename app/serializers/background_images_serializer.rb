f5790d9bcf1aca4f2947d7a29adfb58c32b4358d
Patrick Duvall <duvallp208@gmail.com>
2019-07-31 11:22:12 -0600

    Delete unused file

--- app/serializers/background_images_serializer.rb
+++
@@ -1,9 +0,0 @@
-
-# 
-# class BackgroundImagesSerializer
-#   def self.present_images(images)
-#     {"data" =>
-#       images.map{|image|ImageSerializer.new(image).serialized_json}
-#     }
-#   end
-# end
