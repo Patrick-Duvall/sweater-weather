cc98e8e724796faf788d67ca4c18bc6122ae67cb
Patrick Duvall <duvallp208@gmail.com>
2019-07-30 18:23:53 -0600

    Implement rails caching for forecasts and images

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,8 +2,10 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     city = City.find_or_create_by(querystring: params['location'])
-    forecast_info = DarkskyForecastService.forecast(city.lat,city.lng)
-    forecast = Forecast.new(forecast_info, params['location'])
+    weather = Rails.cache.fetch("forecast-params#{['location']}", expires_in: 15.minutes) do
+      DarkskyForecastService.forecast(city.lat,city.lng)
+    end
+    forecast = Forecast.new(weather, params['location'])
     forecast_to_present = ForecastSerializer.present_forecast(forecast)
     render json: forecast_to_present, status: 200
   end
