c938a70472367cde9ca27f2b6e4f49dfdc3a4d09
Patrick Duvall <duvallp208@gmail.com>
2019-07-31 11:24:20 -0600

    minor cleanup

--- app/models/directions_time.rb
+++ app/models/directions_time.rb
@@ -1,6 +1,6 @@
 class DirectionsTime
-
   attr_reader :seconds, :time_string
+  
   def initialize(info)
     @seconds = info['routes'].first['legs'].first['duration']['value']
     @time_string = info['routes'].first['legs'].first['duration']['text']
